31e1b2736fd93f9caf30367dc5c367d258a785a7
Hassan Hashmi <hashmihassan47@gmail.com>
2023-12-25 09:05:30 -0500

    ad complete

--- src/views/Member/Member.tsx
+++ src/views/Member/Member.tsx
@@ -1,17 +1,19 @@
 import { Positions } from '../../common/interfaces/IPlayer'
 import MemberCourt from './MemberCourt'
+import MobileAd from './MobileAd'
 
 
 export const Member = () => {
   return (
     <div
-      className='flex flex-col max-w-sm items-center border border-black p-4 rounded-lg h-full m-4'
+      className='flex flex-col max-w-sm items-center border border-black p-4 rounded-lg h-full m-4 ml-16'
     >
       <span
         className='text-2xl font-bold'
       >Member View</span>
       <div
         className='flex flex-col space-y-8'>
+        <MobileAd />
         <MemberCourt position={Positions.Court1} />
         <MemberCourt position={Positions.Court2} />
         <MemberCourt position={Positions.Court3} />
