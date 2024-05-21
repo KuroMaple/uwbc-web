faf57ee42f56969fa1b6b263e87655f366aed58a
Hassan Hashmi <hhashmi@janacorporation.com>
2024-05-21 18:01:54 -0400

    fix msw issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,17 @@ import './index.css'
 import App from './app/App'
 import { Provider } from 'react-redux'
 import { store } from './app/redux/store'
-import { handlers } from './__mocks__/handler'
 
 
 async function enableMocking() {
   // Worker is alwasy mocking, TO DO: add logic to only mock in dev
   
-  const { worker } = await import('../src/__mocks__/browser')
+  const { worker } = await import('./__mocks__/browser')
  
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  await worker.start()
-  worker.use(...handlers)
-  return 
+
+  return worker.start()
 }
  
 enableMocking().then(() => {
