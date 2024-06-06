9ad66617bc30e946e5fc1f97f803f5ff32f9fe3c
Hassan Hashmi <hashmihassan47@gmail.com>
2024-06-06 13:58:14 -0400

    feat: autocomplete componenet no connectvity

---
+++ src/common/components/Modal/Modal.tsx
@@ -0,0 +1,55 @@
+import MuiModal from '@mui/material/Modal'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../../app/redux/store'
+import { setModalOpen } from '../../../app/redux/addPlayerModalSlice'
+import Button from '../Button/Button'
+import { Close } from '@mui/icons-material'
+import { Box, SxProps } from '@mui/material'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const modalContentStyles: SxProps = {
+  transform: 'translate(-50%, -50%)',
+  position: 'absolute' as const,
+  top: '50%',
+  left: '50%',
+  width: '400px',
+  height: '500px',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+
+const Modal = ({ children }: Props) => {
+
+  // Redux operations
+  const dispatch = useDispatch()
+  const modalOpen = useSelector((state: RootState) => state.addPlayerModal.open)
+  const handleModalClose = () => dispatch(setModalOpen(false))
+
+  return (
+    <MuiModal
+      open={modalOpen}
+      onClose={handleModalClose}
+      
+    >
+      <Box
+        sx={modalContentStyles}
+      >
+        {children}
+        <div className='absolute top-[8px] right-2'>
+          <Button
+            variant='icon'
+            icon={<Close />}
+            onClick={handleModalClose}
+          />
+        </div>
+      </Box>
+    </MuiModal>
+  )
+}
+
+export default Modal
--- src/common/components/SearchModal/SearchModal.tsx
+++ src/common/components/SearchModal/SearchModal.tsx
@@ -1,42 +1,20 @@
-import { Box, SxProps, TextField, Typography } from '@mui/material'
-import MuiModal from '@mui/material/Modal'
-import { useDispatch, useSelector } from 'react-redux'
-import { setModalOpen } from '../../../app/redux/addPlayerModalSlice'
 import { RootState } from '../../../app/redux/store'
-import Autocomplete from '@mui/material/Autocomplete'
-import IAutoCompleteOption from '../../interfaces/IAutoCompleteOption'
 import { useAddPlayerToSessionMutation } from '../../../services/apis/players'
 import { useGetActiveMembersNotInSessionQuery } from '../../../services/apis/members'
-import { useState } from 'react'
 import './SearchModal.css'
-import Button from '../Button/Button'
-import { Close } from '@mui/icons-material'
+import Modal from '../Modal/Modal'
+import { useSelector } from 'react-redux'
+import Autocomplete from '../Autocomplete/Autocomplete'
+import Typography from '@mui/material/Typography/Typography'
 
 
-const modalContentStyles: SxProps = {
-  transform: 'translate(-50%, -50%)',
-  position: 'absolute' as const,
-  top: '50%',
-  left: '50%',
-  width: '400px',
-  height: '200px',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-}
+
 
 
 
 const SearchModal = () => {
+
   
-  // state
-  const [inputState, setInputState] = useState<IAutoCompleteOption | null>(null) 
-  const [TextFieldState, setTextFieldState] = useState('')
-  // Redux operations
-  const dispatch = useDispatch()
-  const modalOpen = useSelector((state: RootState) => state.addPlayerModal.open)
-  const handleModalClose = () => dispatch(setModalOpen(false))
   const sessionID = useSelector((state: RootState) => state.gym.sessionId) // Current Session id is stored globally
 
 
@@ -46,69 +24,31 @@ const SearchModal = () => {
   //API call 
   const [addPlayer] = useAddPlayerToSessionMutation()
 
-  const addPlayerToSession = (label: IAutoCompleteOption | null) => {
-
-    if (label !== null){
-      const email = label.label.split(' ')[2]
-      addPlayer({session: sessionID, email: email}) // API call to add player to session
-      handleModalClose()
-    }
-    else {
-      console.log('ERROR: No player selected')
-    }
+  const addPlayerToSession = () => {
+    console.log('Adding players to session: ') // Debugging
+    // if (label !== null){
+    //   const email = label.label.split(' ')[2]
+    //   console.log('Adding player to session: ', {
+    //     session: sessionID,
+    //     email: email
+    //   }) // Debugging
+    //   // addPlayer({session: sessionID, email: email}) // API call to add player to session
+    //   // handleModalClose()
+    // }
+    // else {
+    //   console.log('ERROR: No player selected')
+    // }
   }
 
   return (
-    <MuiModal
-      open={modalOpen}
-      onClose={handleModalClose}
-      
-    >
-      <Box
-        sx={modalContentStyles}
-      >
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-            Search for member below:
-        </Typography>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={playerOptions ?? []}
-          sx={{ width: 300 }}
-          renderInput={(params) => 
-            <TextField 
-              {...params} 
-              label="Member Name" 
-              value={TextFieldState}
-              onChange={(event) => {
-                setTextFieldState(event.target.value)
-              }}
-            />}
-
-          value={inputState}
-          onChange={(event, value) => {
-            addPlayerToSession(value)
-            event.preventDefault()
-            setTextFieldState('')
-            setInputState(null)
-          }}
-
-          onSelect= { () => {
-            setTextFieldState('')
-            setInputState(null)            
-          }}
-
-            
-        />
-        <div className='absolute top-[8px] right-2'>
-          <Button
-            variant='icon'
-            icon={<Close />}
-            onClick={handleModalClose}
-          />
-        </div>
-      </Box>
-    </MuiModal>
+    <Modal>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Search for members to add below:
+      </Typography>
+      <Autocomplete
+        options={playerOptions ?? []}
+      />
+    </Modal>
 
   )
 }
